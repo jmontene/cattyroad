2ff9e741498265b7d9c5d15e2323904d8f6328d7
andres <aemc.64@gmail.com>
2017-05-20 17:17:43 -0400

    Random obstacles

--- Assets/Scripts/ObstacleFiller.cs
+++ Assets/Scripts/ObstacleFiller.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class ObstacleFiller : MonoBehaviour {
 
 	public GameObject[] obstacles;
-	public int amount;
+	public int obstaclesAmount;
+	private RowFiller _rowFiller;
+	private int _units;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_units = (int)transform.localScale.x;
+		_rowFiller = GetComponent<RowFiller> ();
+		int fillAmount = 0;
+		if (_rowFiller != null)
+			fillAmount = _rowFiller.fillAmount;
+		List<int> indexesAvailable = new List<int> ();
+		for (int i = 0 + fillAmount; i < _units - fillAmount; i++)
+			indexesAvailable.Add (i);
+		indexesAvailable.Shuffle ();
+		GameObject obs;
+		int randomIdx;
+		for (int i = 0; i < obstaclesAmount && i < _units - fillAmount * 2; i++)
+		{
+			randomIdx = Random.Range (0, obstacles.Length - 1);
+			obs = Instantiate (obstacles [randomIdx]);
+			obs.transform.position = new Vector3 (indexesAvailable[i], obs.transform.position.y, transform.position.z);
+			obs.transform.SetParent (transform);
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/RowFiller.cs
+++ Assets/Scripts/RowFiller.cs
@@ -12,21 +12,16 @@ public class RowFiller : MonoBehaviour {
 	{
 		_units = (int)transform.localScale.x;
 		GameObject fillObj;
-		Vector3 pos;
 		int randomIdx;
 		for (int i = 0; i < fillAmount; i++)
 		{
 			randomIdx = Random.Range (0, fillingObjects.Length - 1);
 			fillObj = Instantiate (fillingObjects [randomIdx]);
-			pos = fillObj.transform.position;
-			pos.x = i;
-			fillObj.transform.localPosition = pos;
+			fillObj.transform.position = new Vector3 (i, fillObj.transform.position.y, transform.position.z);
 			fillObj.transform.SetParent (transform);
 			randomIdx = Random.Range (0, fillingObjects.Length - 1);
 			fillObj = Instantiate (fillingObjects [randomIdx]);
-			pos = fillObj.transform.position;
-			pos.x = _units - 1 - i;
-			fillObj.transform.localPosition = pos;
+			fillObj.transform.position = new Vector3 (_units - 1 - i, fillObj.transform.position.y, transform.position.z);
 			fillObj.transform.SetParent (transform);
 		}
 	}
