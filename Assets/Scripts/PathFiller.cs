b9c28cba609679c7ed4eb4f252611bcf32e54ef5
andres <aemc.64@gmail.com>
2017-05-21 20:04:40 -0400

    Fixing Pathfiller

--- Assets/Scripts/PathFiller.cs
+++ Assets/Scripts/PathFiller.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFiller : MonoBehaviour {
+public class PathFiller : Assigner {
 
 	public GameObject[] paths;
 	public int pathsAmount;
-	public float continuousPaths;
+	public int continuousPaths = 1;
 	private BorderFiller _rowFiller;
 	private int _units;
+	private int[] _continuousPathIdx;
 
-	void Start ()
+	void Awake ()
 	{
+		_continuousPathIdx = new int[continuousPaths];
 		_units = (int)transform.localScale.x;
 		_rowFiller = GetComponent<BorderFiller> ();
+	}
+
+	public override void Assign()
+	{
 		int fillAmount = 0;
 		if (_rowFiller != null)
 			fillAmount = _rowFiller.fillAmount;
@@ -23,30 +29,48 @@ public class PathFiller : MonoBehaviour {
 		indexesAvailable.Shuffle ();
 		int randomIdx;
 		GameObject obs;
-
-		Transform previousRow = RowController.Instance.PreviousRow;
-		if (previousRow != null && previousRow.GetComponent<PathFiller> ())
+		Transform previousRow = RowController.Instance.GetPreviousRow(0);
+		if (previousRow != null)
 		{
-			Transform[] previousPaths = previousRow.GetInmediateChildren ().ToArray ();
-			for (int i = 0; i < continuousPaths && i < previousPaths.Length; i++)
+			PathFiller pf = previousRow.GetComponent<PathFiller> ();
+			if (pf != null)
 			{
-				randomIdx = Random.Range (0, previousPaths.Length);
-				Transform previousPath = previousPaths [randomIdx];
-				obs = Instantiate (paths [randomIdx]);
-				float xPos = previousPath.position.x;
-				obs.transform.position = new Vector3 (xPos, obs.transform.position.y, transform.position.z);
-				obs.transform.SetParent (transform);
-				indexesAvailable.Remove ((int)xPos);
-				pathsAmount--;
+				Transform previousRow2 = RowController.Instance.GetPreviousRow(0);
+
+
+				Transform[] previousPaths = previousRow.GetInmediateChildren ().ToArray ();
+				List<int> continuousAvailable = new List<int> ();
+				for (int i = 0; i < pf._continuousPathIdx.Length; i++)
+					continuousAvailable.Add (i);
+				continuousAvailable.Shuffle ();
+				for (int i = 0; i < _continuousPathIdx.Length && i < pf._continuousPathIdx.Length; i++)
+				{
+					Transform previousPath = previousPaths [continuousAvailable[i]];
+					randomIdx = Random.Range (0, paths.Length);
+					obs = Instantiate (paths [randomIdx]);
+					float xPos = previousPath.position.x;
+					obs.transform.position = new Vector3 (xPos, obs.transform.position.y, transform.position.z);
+					obs.transform.SetParent (transform);
+					indexesAvailable.Remove ((int)xPos);
+					pathsAmount--;
+					_continuousPathIdx [_continuousPathIdx.Length - continuousPaths] = continuousAvailable [i];
+					continuousPaths--;
+					continuousAvailable.Remove (continuousAvailable [i]);
+				}
 			}
 		}
-
 		for (int i = 0; i < pathsAmount && i < _units - fillAmount * 2; i++)
 		{
 			randomIdx = Random.Range (0, paths.Length);
 			obs = Instantiate (paths [randomIdx]);
 			obs.transform.position = new Vector3 (indexesAvailable[i], obs.transform.position.y, transform.position.z);
 			obs.transform.SetParent (transform);
+			if (continuousPaths > 0)
+			{
+				_continuousPathIdx [_continuousPathIdx.Length - continuousPaths] = indexesAvailable [i];
+				continuousPaths--;
+			}
 		}
+
 	}
 }
